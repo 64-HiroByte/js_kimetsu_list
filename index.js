65dae0b26c1ec692e78e7cf31105a7004eed2355
64-HiroByte <higher.ground.rc@gmail.com>
2025-09-07 15:58:08 +0900

    htmlのレイアウトを作成した

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const url = "https://ihatov08.github.io/kimetsu_api/api/all.json";
 
 // fetchでAPIを叩く（リクエストを送る）
-fetch(url)
-  .then(response => response.json())  // JSONデータに変換
-  .then(data => {
-    // dataにキャラの一覧が入ってくる
-    console.log(data);
+// fetch(url)
+//   .then((response) => response.json()) // JSONデータに変換
+//   .then((data) => {
+//     // dataにキャラの一覧が入ってくる
+//     console.log(data);
 
-    const list = document.getElementById("character-list");
+//     const list = document.getElementById("character-list");
 
-    // 例えば名前だけ表示する
-    data.forEach(character => {
-      const li = document.createElement("li");
-      li.textContent = character.name; // キャラ名
-      list.appendChild(li);
-    });
-  })
-  .catch(error => {
-    console.error("エラー:", error);
-  });
+//     // 例えば名前だけ表示する
+//     data.forEach((character) => {
+//       const li = document.createElement("li");
+//       li.textContent = character.name; // キャラ名
+//       list.appendChild(li);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("エラー:", error);
+//   });

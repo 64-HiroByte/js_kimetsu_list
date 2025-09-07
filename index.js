f0c30a492b0246502e4b5253982f0a0d7eb71e60
64-HiroByte <higher.ground.rc@gmail.com>
2025-09-08 00:01:50 +0900

    DOM操作を行う処理を追加した。

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { element, render } from "./html-util.js";
+
 const BASE_URL = "https://ihatov08.github.io";
 
 const API_URLS = {
@@ -19,9 +21,31 @@ const getImageUrl = (res) => {
   return BASE_URL + res.image;
 };
 
-const fetchCharactor = async () => {
+const createElement = (character) => {
+  return element`
+    <div class="col">
+      <div class="card h-100 bg-dark shadow">
+        <img
+          src="${getImageUrl(character)}"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title text-center text-warning fst-italic">
+            ${character.category}
+          </h5>
+          <h4 class="card-title text-center text-light fw-bold mt-3 mb-4">
+            ${character.name}
+          </h4>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+const fetchcharacter = async () => {
   const apiUrl = getApiUrl();
-  const spinner = document.getElementById("spinner");
+  const spinner = document.getElementById("js-spinner");
   spinner.classList.remove("d-none");
   try {
     const response = await fetch(apiUrl);
@@ -30,13 +54,17 @@ const fetchCharactor = async () => {
         `データを取得できませんでした（ステータスコード: ${response.status}）`
       );
     }
-    const charactors = await response.json();
+    const characters = await response.json();
 
-    charactors.forEach((charactor) => {
-      console.log("画像: ", getImageUrl(charactor));
-      console.log("カテゴリー: ", charactor.category);
-      console.log("名前: ", charactor.name);
+    const characterList = document.getElementById("js-character-list");
+
+    const fragment = document.createDocumentFragment();
+
+    characters.forEach((character) => {
+      const characterElement = createElement(character);
+      fragment.appendChild(characterElement);
     });
+    render(fragment, characterList);
   } catch (error) {
     console.error(error.message);
   } finally {
@@ -44,7 +72,7 @@ const fetchCharactor = async () => {
   }
 };
 
-fetchCharactor();
+fetchcharacter();
 
-const radios = document.getElementById("js-radio-selector");
-radios.addEventListener("change", fetchCharactor);
+const radios = document.getElementById("js-radios");
+radios.addEventListener("change", fetchcharacter);

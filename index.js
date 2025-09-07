50454ec7b4c3a7ff85d319b21d08b64ff92a2bd8
64-HiroByte <higher.ground.rc@gmail.com>
2025-09-07 18:48:11 +0900

    APIからデータ取得する非同期処理とラジオボタン選択時のイベントを追加した

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const url = "https://ihatov08.github.io/kimetsu_api/api/all.json";
+const BASE_URL = "https://ihatov08.github.io";
 
-// fetchでAPIを叩く（リクエストを送る）
-// fetch(url)
-//   .then((response) => response.json()) // JSONデータに変換
-//   .then((data) => {
-//     // dataにキャラの一覧が入ってくる
-//     console.log(data);
+const API_URLS = {
+  all: "https://ihatov08.github.io/kimetsu_api/api/all.json",
+  hashira: "https://ihatov08.github.io/kimetsu_api/api/hashira.json",
+  oni: "https://ihatov08.github.io/kimetsu_api/api/oni.json",
+  kisatsutai: "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json",
+};
 
-//     const list = document.getElementById("character-list");
+const categories = document.forms[0].elements["category"];
 
-//     // 例えば名前だけ表示する
-//     data.forEach((character) => {
-//       const li = document.createElement("li");
-//       li.textContent = character.name; // キャラ名
-//       list.appendChild(li);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("エラー:", error);
-//   });
+// APIのURLを取得
+const getApiUrl = () => {
+  const category = categories.value;
+  return API_URLS[category];
+};
+
+const getImageUrl = (res) => {
+  return BASE_URL + res.image;
+};
+
+const fetchCharactor = async () => {
+  const apiUrl = getApiUrl();
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("d-none");
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(
+        `データを取得できませんでした（ステータスコード: ${response.status}）`
+      );
+    }
+    const charactors = await response.json();
+
+    charactors.forEach((charactor) => {
+      console.log("画像: ", getImageUrl(charactor));
+      console.log("カテゴリー: ", charactor.category);
+      console.log("名前: ", charactor.name);
+    });
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    spinner.classList.add("d-none");
+  }
+};
+
+fetchCharactor();
+
+const radios = document.getElementById("js-radio-selector");
+radios.addEventListener("change", fetchCharactor);
